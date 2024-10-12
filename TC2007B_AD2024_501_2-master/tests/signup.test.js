6441c8e802e35753805d248a13e3f44d08f010c3
Jessica Odette Guizado Hernández <A01643724@tec.mx>
2024-10-11 22:12:15 -0600

    sí pasan los test

--- TC2007B_AD2024_501_2-master/tests/signup.test.js
+++ TC2007B_AD2024_501_2-master/tests/signup.test.js
@@ -1,12 +1,10 @@
 import '@testing-library/jest-native/extend-expect';
 import React from 'react';
 import { render, fireEvent, waitFor } from '@testing-library/react-native';
-import  SignUp  from '../views/singup';
+import SignUp from '../views/singup';
 import { AuthContext } from '../views/authentication';
 import { createUserWithEmailAndPassword } from 'firebase/auth';
 
-
-// Mock the Firebase auth function
 jest.mock('firebase/auth', () => ({
   getAuth: jest.fn(() => ({
     currentUser: { uid: 'test-uid', email: 'test@example.com' }
@@ -16,15 +14,18 @@ jest.mock('firebase/auth', () => ({
 }));
 
 describe('SignUp Component', () => {
-  const mockAuth = {}; // Mock authentication object
+  const mockAuth = {}; 
+
+  beforeAll(() => {
+    global.alert = jest.fn();  
+  });
 
   it('should sign up a user with valid email and password', async () => {
-    // Mock the resolved value of createUserWithEmailAndPassword
     const mockCreateUser = createUserWithEmailAndPassword.mockResolvedValueOnce({
       user: { email: 'test@example.com' },
     });
 
-    const { getByPlaceholderText, getByText } = render(
+    const { getByPlaceholderText, getByRole } = render(
       <AuthContext.Provider value={{ auth: mockAuth }}>
         <SignUp />
       </AuthContext.Provider>
@@ -32,42 +33,38 @@ describe('SignUp Component', () => {
 
     const emailInput = getByPlaceholderText('Correo');
     const passwordInput = getByPlaceholderText('Contraseña');
-    const signUpButton = getByText('Sign Up');
+    const signUpButton = getByRole('button', { name: /Sign Up/i });
 
-    // Simulate user input
+
     fireEvent.changeText(emailInput, 'test@example.com');
     fireEvent.changeText(passwordInput, 'password123');
     fireEvent.press(signUpButton);
 
-    // Verify the createUserWithEmailAndPassword function was called correctly
     await waitFor(() => {
       expect(mockCreateUser).toHaveBeenCalledWith(mockAuth, 'test@example.com', 'password123');
     });
   });
 
   it('should show an alert for a weak password', async () => {
-    // Mock the rejection of createUserWithEmailAndPassword for a weak password
     const mockCreateUser = createUserWithEmailAndPassword.mockRejectedValueOnce({
       code: 'auth/weak-password',
       message: 'The password is too weak.',
     });
 
-    const { getByPlaceholderText, getByText } = render(
+    const { getByPlaceholderText, getByRole } = render(
       <AuthContext.Provider value={{ auth: mockAuth }}>
         <SignUp />
       </AuthContext.Provider>
     );
 
-    const emailInput = getByPlaceholderText('Email');
-    const passwordInput = getByPlaceholderText('Password');
-    const signUpButton = getByText('Sign Up');
+    const emailInput = getByPlaceholderText('Correo');
+    const passwordInput = getByPlaceholderText('Contraseña');
+    const signUpButton = getByRole('button', { name: /Sign Up/i });
 
-    // Simulate user input
     fireEvent.changeText(emailInput, 'test@example.com');
     fireEvent.changeText(passwordInput, '123');
     fireEvent.press(signUpButton);
 
-    // Verify the alert is shown for a weak password
     await waitFor(() => {
       expect(alert).toHaveBeenCalledWith('That password is too weak!');
     });
